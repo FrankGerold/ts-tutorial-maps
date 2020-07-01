6db639b3d7497b482a2ae7e6013d29b119cccdf8
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-01 12:24:57 -0400

    Added Implementation clauses for readability and error clarity!

--- src/Company.ts
+++ src/Company.ts
@@ -1,6 +1,8 @@
 import faker from "faker";
 
-export class Company {
+import { Mappable } from "./Map";
+
+export class Company implements Mappable {
   name: string;
   catchPhrase: string;
   location: {
