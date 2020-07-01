493f04633bc342d5faae7cccc119e28cb83d7bb1
Frank Gerold <FrankETSGerold@gmail.com>
2020-06-30 20:46:47 -0400

    company class definition

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 console.log('Ayy lmao');
 
 import { User } from "./User";
+import { Company } from "./Company";
 
 
 const user = new User();
-
 console.log(user);
+
+const comp = new Company();
+console.log(comp);
